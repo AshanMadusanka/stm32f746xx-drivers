017b52dcfde8b9452a212ce2a5b272f972406f12
Ashan <2017t00036@stu.cmb.ac.lk>
2025-05-26 19:17:51 +0530

    Add Documentation

--- drivers/Src/stm32f746xx_gpio_driver.c
+++ drivers/Src/stm32f746xx_gpio_driver.c
@@ -3,13 +3,20 @@
  *
  *  Created on: May 24, 2025,
  *      Author: Ashan
+ *
+ *  This file contains the implementation of GPIO driver functions for the STM32F746xx microcontroller.
  */
 
 #include "stm32f746xx_gpio_driver.h"
 #include "stm32f746xx.h"
 
-/*Peripheral Clock setup*/
-void GPIO_PeriClockControl(GPIO_RegDef_t *pGPIOx, uint8_t EnorDi){
+/**
+ * @brief  Enables or disables the peripheral clock for the given GPIO port.
+ * @param  pGPIOx: Pointer to the GPIO port base address.
+ * @param  EnorDi: ENABLE to enable the clock, DISABLE to disable the clock.
+ * @retval None
+ */
+void GPIO_PeriClockControl(GPIO_RegDef_t *pGPIOx, uint8_t EnorDi) {
 
     if(EnorDi == ENABLE){
 
@@ -112,8 +119,11 @@ void GPIO_PeriClockControl(GPIO_RegDef_t *pGPIOx, uint8_t EnorDi){
   }
 }
 
-/*Init and De Init*/
-
+/**
+ * @brief  Initializes the GPIO pin with the specified configuration.
+ * @param  pGPIOHandle: Pointer to the GPIO handle structure containing pin configuration.
+ * @retval None
+ */
 void GPIO_Init(GPIO_Handle_t *pGPIOHandle) {
 
    uint32_t temp = 0;
@@ -157,6 +167,12 @@ void GPIO_Init(GPIO_Handle_t *pGPIOHandle) {
         }
     }
 }
+
+/**
+ * @brief  Resets the GPIO port to its default state.
+ * @param  pGPIOx: Pointer to the GPIO port base address.
+ * @retval None
+ */
 void GPIO_DeInit(GPIO_RegDef_t *pGPIOx) {
 
     if (pGPIOx == GPIOA) {
@@ -205,7 +221,12 @@ void GPIO_DeInit(GPIO_RegDef_t *pGPIOx) {
     }
 }
 
-/*Data Read and Write*/
+/**
+ * @brief  Reads the value of a specific GPIO input pin.
+ * @param  pGPIOx: Pointer to the GPIO port base address.
+ * @param  PinNumber: Pin number to read.
+ * @retval uint8_t: Value of the pin (0 or 1).
+ */
 uint8_t GPIO_ReadFromInputPin(GPIO_RegDef_t *pGPIOx, uint8_t PinNumber) {
 
 
@@ -213,11 +234,24 @@ uint8_t GPIO_ReadFromInputPin(GPIO_RegDef_t *pGPIOx, uint8_t PinNumber) {
     value = (uint8_t)pGPIOx->IDR >> PinNumber & 0x00000001;
     return value;
 }
+
+/**
+ * @brief  Reads the value of the entire GPIO input port.
+ * @param  pGPIOx: Pointer to the GPIO port base address.
+ * @retval uint16_t: Value of the input port.
+ */
 uint16_t GPIO_ReadFromInputPort(GPIO_RegDef_t *pGPIOx) {
     uint16_t value = (uint16_t) pGPIOx->IDR;
     return value;
 }
 
+/**
+ * @brief  Writes a value to a specific GPIO output pin.
+ * @param  pGPIOx: Pointer to the GPIO port base address.
+ * @param  PinNumber: Pin number to write to.
+ * @param  Value: Value to write (GPIO_PIN_SET or GPIO_PIN_RESET).
+ * @retval None
+ */
 void GPIO_WriteToOutputPin(GPIO_RegDef_t *pGPIOx, uint8_t PinNumber, uint8_t Value) {
 
     if(Value == GPIO_PIN_SET) {
@@ -228,23 +262,50 @@ void GPIO_WriteToOutputPin(GPIO_RegDef_t *pGPIOx, uint8_t PinNumber, uint8_t Val
         pGPIOx->ODR &=~(1 << PinNumber);
     }
 }
+
+/**
+ * @brief  Writes a value to the entire GPIO output port.
+ * @param  pGPIOx: Pointer to the GPIO port base address.
+ * @param  Value: Value to write to the port.
+ * @retval None
+ */
 void GPIO_WriteToOutputPort(GPIO_RegDef_t *pGPIOx, uint16_t Value) {
 
     pGPIOx->ODR = Value;
 
 }
+
+/**
+ * @brief  Toggles the state of a specific GPIO output pin.
+ * @param  pGPIOx: Pointer to the GPIO port base address.
+ * @param  PinNumber: Pin number to toggle.
+ * @retval None
+ */
 void GPIO_ToggleOutputPin(GPIO_RegDef_t *pGPIOx, uint8_t PinNumber) {
 
     pGPIOx->ODR ^= (1 << PinNumber);
 
 }
 
-/*IRQ Configuration and ISR Handling*/
+/**
+ * @brief  Configures the interrupt for a specific GPIO pin.
+ * @param  IRQNumber: IRQ number of the GPIO pin.
+ * @param  IRQPriority: Priority of the interrupt.
+ * @param  EnorDi: ENABLE to enable the interrupt, DISABLE to disable it.
+ * @retval None
+ */
 void GPIO_IRQInterruptConfig(uint8_t IRQNumber,uint8_t IRQPriority, uint8_t EnorDi) {
 
 
 }
+
+/**
+ * @brief  Handles the interrupt for a specific GPIO pin.
+ * @param  PinNumber: Pin number that triggered the interrupt.
+ * @retval None
+ */
 void GPIO_IRQHandler(uint8_t PinNumber) {
 
 
 }
+
